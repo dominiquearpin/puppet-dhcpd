2c2af18746b18087a0d2568bfa1eadffc5fa63a4
Jonathan Gazeley <me@jonathangazeley.com>
2015-07-06 15:19:31 +0100

    Test config before restarting daemon

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,7 +1,20 @@
 class dhcpd::service {
   include dhcpd::params
   service { $::dhcpd::params::service_name:
-    ensure => running,
-    enable => true,
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
   }
+
+  # This exec tests the dhcpd config and fails if it's bad
+  # It isn't run every time puppet runs, but only when dhcpd is to be restarted
+  exec { 'dhcpd-config-test':
+    command     => '/usr/bin/sudo /usr/sbin/dhcpd -q -t',
+    returns     => 0,
+    refreshonly => true,
+    logoutput   => on_failure,
+    notify      => Service[$::dhcpd::params::service_name],
+  }
+
 }
